0d3beb0693e752fa14ac9999d6ee32c33c210b8e
Lucas Borges <lbcarmo08@gmail.com>
2025-06-20 15:28:51 -0300

    Leitor dos coeficientes e termos atualizado

--- main.c
+++ main.c
@@ -1,15 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main()
-{
-    int quantidade=0,tamanho=0;
-    float e=0.0;
-
-    FILE *arquivo = fopen("arquivo.txt", "r");
-    fscanf(arquivo, "%i %i %f",&quantidade,&tamanho,&e);
-    float matriz[tamanho][tamanho],valores[tamanho];
-
+void lerMatriz(float** matriz,float *termos,int tamanho,FILE* arquivo){
     /*Ler Matriz dos Coeficientes*/
     for(int x=0;x<tamanho;x++){
         for(int y=0;y<tamanho;y++){
@@ -19,17 +11,32 @@ int main()
 
     /*Ler Vetor dos Termos Independentes*/
     for(int x=0;x<tamanho;x++){
-        fscanf(arquivo,"%f",&valores[x]);
+        fscanf(arquivo,"%f",&termos[x]);
     }
+}
+
+int main()
+{
+    int quantidade=0,tamanho=0;
+    float e=0.0;
+
+    FILE *arquivo = fopen("arquivo.txt", "r");
+    fscanf(arquivo, "%i %i %f",&quantidade,&tamanho,&e);
+    float termos[tamanho];
+    float** matriz = (float**) malloc(tamanho*sizeof(float*));
+    for(int x=0;x<tamanho;x++){
+        matriz[x] = (float*) malloc(tamanho*sizeof(float));
+    }
+
+    lerMatriz(matriz,termos,tamanho,arquivo);
 
     /**Print de Teste*/
     for(int x=0;x<tamanho;x++){
         for(int y=0;y<tamanho;y++){
             printf("%.3f ",matriz[x][y]);
         }
-        printf("= %f\n",valores[x]);
+        printf("= %f\n",termos[x]);
     }
-
 
     return 0;
 }
