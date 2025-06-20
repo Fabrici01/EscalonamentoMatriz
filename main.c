3a5f633bcef9bb60d48b1240b96cabf30ec7a3d8
Lucas Borges <lbcarmo08@gmail.com>
2025-06-20 15:09:56 -0300

    Leitor dos Arquivos

--- main.c
+++ main.c
@@ -3,7 +3,33 @@
 
 int main()
 {
-    /* code */
+    int quantidade=0,tamanho=0;
+    float e=0.0;
+
+    FILE *arquivo = fopen("arquivo.txt", "r");
+    fscanf(arquivo, "%i %i %f",&quantidade,&tamanho,&e);
+    float matriz[tamanho][tamanho],valores[tamanho];
+
+    /*Ler Matriz dos Coeficientes*/
+    for(int x=0;x<tamanho;x++){
+        for(int y=0;y<tamanho;y++){
+            fscanf(arquivo,"%f",&matriz[x][y]);
+        }
+    }
+
+    /*Ler Vetor dos Termos Independentes*/
+    for(int x=0;x<tamanho;x++){
+        fscanf(arquivo,"%f",&valores[x]);
+    }
+
+    /**Print de Teste*/
+    for(int x=0;x<tamanho;x++){
+        for(int y=0;y<tamanho;y++){
+            printf("%.3f ",matriz[x][y]);
+        }
+        printf("= %f\n",valores[x]);
+    }
+
+
     return 0;
-    
 }
