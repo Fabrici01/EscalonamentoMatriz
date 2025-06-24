bad466d927f22d841fd08c127a4049f229e1491e
Lucas Borges <lbcarmo08@gmail.com>
2025-06-24 08:30:49 -0300

    Pivoteamento da Eliminação de Gauss corrigido

--- EliminacaoGauss.c
+++ EliminacaoGauss.c
@@ -4,27 +4,26 @@
 
 void resolverEliminacaoGauss(float** matriz, float* termo, int tamanho){
     float m=0.0,vetorTemp[tamanho],termoTemp=0.0,resultado[tamanho];
-    int i=0;
+    int indiceMaior=0;
 
     //Eliminação de Gauss em matrizU
     for(int k=0;k<tamanho;k++){
-        if(matriz[k][k]==0){
-            i = k+1;
-            while(i<tamanho&&matriz[i][k]==0)i++;
-            if(i<tamanho){
-                for(int x=0;x<tamanho;x++){
-                    vetorTemp[x] = matriz[k][x];
-                    matriz[k][x] = matriz[i][x];
-                    matriz[i][x] = vetorTemp[x];
-                }
-                termoTemp = termo[k];
-                termo[k] = termo[i];
-                termo[i] = termoTemp;
-            }else{
-                printf("Matriz sem solucao definida!\n");
-                exit(0);
-            }
+        indiceMaior = k;
+        for(int i=k;i<tamanho;i++){
+            if(matriz[i][k]>matriz[indiceMaior][k]) indiceMaior = i;
         }
+        if(matriz[indiceMaior][k]==0){
+            printf("Matriz sem solucao definida!\n");
+            exit(1);
+        }
+        for(int x=0;x<tamanho;x++){
+            vetorTemp[x] = matriz[k][x];
+            matriz[k][x] = matriz[indiceMaior][x];
+            matriz[indiceMaior][x] = vetorTemp[x];
+        }
+            termoTemp = termo[k];
+            termo[k] = termo[indiceMaior];
+            termo[indiceMaior] = termoTemp;
         for(int l=k+1;l<tamanho;l++){
             m = matriz[l][k]/matriz[k][k];
             for(int x=k;x<tamanho;x++){
